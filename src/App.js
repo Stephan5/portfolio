9852bd66cfb9f62fdefb3e48c2b851056914d8c7
Stephan <stephan.github@blakeslee.uk>
2018-07-15 17:21:23 +0300

    Add landing page.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="App-header"><h1 className="App-title">Stephan Blakeslee</h1>
+          <p className="App-intro">
+            Site under construction
+          </p>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
