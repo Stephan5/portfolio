fc6660f96e879436a89da8043c2b1f9ed30e345c
Stephan <stephan.github@blakeslee.uk>
2018-07-15 15:58:11 +0300

    Initial CRA Commit

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Performs a release of a new version of the site. This script will fail
+# if the working directory is not clean (in git terms).
+rm -rf deploy build
+
+# install dependencies and run build
+npm run build
+
+mkdir deploy
+cd deploy
+cp -r ../build ./static
+cp -r ../public/* ./static
+cp -r ../index.html .
+cp -r ../error.html .
+cd ..
+
+rm -rf build
