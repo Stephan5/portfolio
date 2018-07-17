b8419d4dc99e0efc0327595becd998d8e28e183b
Stephan <stephan.github@blakeslee.uk>
2018-07-17 23:10:44 +0300

    Add starling api

--- src/components/FormError/FormError.js
+++ src/components/FormError/FormError.js
@@ -3,6 +3,5 @@ import styles from "./FormError.scss";
 
 export const FormError = ({ errors }) => {
   const message = errors ? Object.values(errors) : "";
-  console.warn(message);
   return <div className={styles.formError}>{message}</div>;
 };
